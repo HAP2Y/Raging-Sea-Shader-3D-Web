fa444b82c6d85c72bc35632fb6c090b2793401eb
Happy Patel <happyp999@gmail.com>
2023-04-24 20:41:34 -0400

    adding project

---
+++ src/shaders/water/fragment.glsl
@@ -0,0 +1,17 @@
+// Uniforms
+// Color
+uniform vec3 uDepthColor;
+uniform vec3 uSurfaceColor;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
+
+// Varyings
+varying float vElevation;
+
+void main(){
+    // Set the color based on the elevation
+    float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
+    vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
+
+    gl_FragColor = vec4(color, 1.0);
+}
